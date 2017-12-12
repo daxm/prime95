06f2ae86a4aeb651e6a9fa9577b001900730ba50
Dax Mickelson <github@daxm.net>
2017-12-12 12:48:27 -0700

    messing around with logging in.

--- ghz-days.py
+++ ghz-days.py
@@ -5,16 +5,18 @@ from userdata import *
 import requests
 
 main_url = 'http://www.mersenne.org/'
-login_url = 'http://www.mersenne.org/account/default.php'
+login_url = 'http://www.mersenne.org'
 login_data = {'user_login': USERNAME, 'user_password': PASSWORD}
 
 with requests.Session() as session:
+    # Access main_url to collect cookies.
     result = session.get(main_url)
-
     print(result.cookies)
+    print(result.headers)
+
+
     result = session.post(login_url,
                           data=login_data,
-                          headers=dict(referer=main_url),
-                          cookies=result.cookies,)
+                          )
     print(result.cookies)
-    print(result.text)
+    print(result.headers)
