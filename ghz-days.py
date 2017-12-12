d08b4b9192c3e8d7503e3d34fd91096bdbfd5a22
Dax Mickelson <github@daxm.net>
2017-12-12 15:18:11 -0700

    Got data, now to work with it to get information...

--- ghz-days.py
+++ ghz-days.py
@@ -3,20 +3,44 @@
 
 from userdata import *
 import requests
+from lxml import html
 
-main_url = 'http://www.mersenne.org/'
-login_url = 'http://www.mersenne.org'
+# ### Possible changeable data ### #
+# results_limit can be anything from '1' to '9999'.
+results_limit = '2'
+# Set account_details to 1 for expanded version or 0 for not.
+account_details = '1'
+# ### #
+
+
+def text(elt):
+    return elt.text_content().replace(u'\xa0', u' ')
+
+
+# Build URLs
+domain = 'www.mersenne.org'
+url = 'https://{}'.format(domain)
+main_url = url
+login_url = '{}/'.format(url)
 login_data = {'user_login': USERNAME, 'user_password': PASSWORD}
+results_url = '{}/results/'.format(url)
+results_params = 'limit={}'.format(results_limit)
+account_url = '{}/account/'.format(url)
+account_params = 'details={}'.format(account_details)
 
 with requests.Session() as session:
-    # Access main_url to collect cookies.
-    result = session.get(main_url)
-    print(result.cookies)
-    print(result.headers)
+    # Log in
+    session.post(login_url, data=login_data,)
 
-
-    result = session.post(login_url,
-                          data=login_data,
-                          )
-    print(result.cookies)
-    print(result.headers)
+    # Get and format needed data.
+    result = session.get(results_url, params='limit={}'.format(results_limit))
+    tree = html.fromstring(result.content)
+    # I'm not fully sure what all this next block of code does but it certainly cleans up my dataset!
+    # https://stackoverflow.com/questions/28305578/python-get-html-table-data-by-xpath
+    for table in tree.xpath('//table[@id="report1"]'):
+        header = [text(th) for th in table.xpath('//th')]  # 1
+        data = [[text(td) for td in tr.xpath('td')]
+                for tr in table.xpath('//tr')]  # 2
+        data = [row for row in data if len(row) == len(header)]  # 3
+        # data = pd.DataFrame(data, columns=header)  # 4
+        print(data)
--- readme.md
+++ readme.md
@@ -1,12 +1,19 @@
-# Welcome to the Prime95 project!
+# Welcome to the Prime95 Stats project!
 
 The goal of this project is to allow user's of the Great Internet Mersenne Prime Search (GIMPS) to gather statistics of
 their efforts on this project.
 
-**NOTE:  I purposely didn't add the userdata.py file as that is where your username and password should be stored in that
-file.  Create a userdata.py file and add the following two lines:**
+NOTE:  I purposely didn't add the userdata.py file as that is where your username and password should be stored in that
+file.  Create a userdata.py file and add the following two lines:
 ```
 USERNAME = 'your_gimps_username'
 PASSWORD = 'your_gimps_password'
 ```
+OR, you can just add the **USERNAME** and **PASSWORD** variables to the top of your ghz-days.py script and comment out the
+following in ghz-days.py:
+```
+from userdata import *
+```
 
+My first goal is to compute the average GHz-days on a daily basis.  (I know, it is weird to think of GHz-days/day but
+that is the units being measured here.)
