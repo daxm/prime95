6aed97ab08ec95bfa749f000e95520592dfa0dcd
daxm <3335783+daxm@users.noreply.github.com>
2017-12-14 19:36:26 -0700

    changed how to compute ranking surpass as well as which to show.

--- my_gimps_stats.py
+++ my_gimps_stats.py
@@ -170,68 +170,25 @@ def main():
 
     # The def parse_table() doesn't handle the Account nor Top 500 web pages well.
     my_rank_365, my_overall_365, my_ghzdays_365 = get_account_stats('report1', account_html)
-    print('My last 365 day Rank: {}/{}'.format(my_rank_365, my_overall_365))
+    print('Your last 365 day Rank: {} out of {} active reports.'.format(my_rank_365, my_overall_365))
     my_rank_lifetime, my_overall_lifetime, my_ghzdays_lifetime = get_account_stats('report2', account_html)
-    print('My Overall Rank: {}/{}'.format(my_rank_lifetime, my_overall_lifetime))
+    print('Your Overall Rank: {} out of {} reported, ever.'.format(my_rank_lifetime, my_overall_lifetime))
 
     # Collect the Top 500 table for comparison parsing vs. my stats.
-    ghzdays_500 = int(float(get_500_level_stats(html_content=top_500_html, rank='500')) * 10000)
-    my_score = int(float(my_ghzdays_lifetime.replace(',', '')) * 10000)
-    days2go = 99999.9999
-    if my_score < ghzdays_500:
-        print('Your rank is smaller than 500.')
-        delta = (ghzdays_500 - my_score) / 10000
-        days2go = delta / daily_average
+    my_score = float(my_ghzdays_lifetime.replace(',', ''))
+    my_rank = int(float(my_overall_lifetime.replace(',', '')))
+    if my_rank > 500:
+        next_rank = '500'
     else:
-        ghzdays_250 = int(float(get_500_level_stats(html_content=top_500_html, rank='250')) * 10000)
-        if my_score < ghzdays_250:
-            print('Your rank is between 500 and 250.')
-            delta = (ghzdays_250 - my_score) / 10000
-            days2go = delta / daily_average
-        else:
-            ghzdays_125 = int(float(get_500_level_stats(html_content=top_500_html, rank='125')) * 10000)
-            if my_score < ghzdays_125:
-                print('Your rank is between 250 and 125.')
-                delta = (ghzdays_125 - my_score) / 10000
-                days2go = delta / daily_average
-            else:
-                ghzdays_100 = int(float(get_500_level_stats(html_content=top_500_html, rank='100')) * 10000)
-                if my_score < ghzdays_100:
-                    print('Your rank is between 125 and 100.')
-                    delta = (ghzdays_100 - my_score) / 10000
-                    days2go = delta / daily_average
-                else:
-                    ghzdays_50 = int(float(get_500_level_stats(html_content=top_500_html, rank='50')) * 10000)
-                    if my_score < ghzdays_50:
-                        print('Your rank is between 100 and 50.')
-                        delta = (ghzdays_50 - my_score) / 10000
-                        days2go = delta / daily_average
-                    else:
-                        ghzdays_25 = int(float(get_500_level_stats(html_content=top_500_html, rank='25')) * 10000)
-                        if my_score < ghzdays_25:
-                            print('Your rank is between 50 and 25.')
-                            delta = (ghzdays_25 - my_score) / 10000
-                            days2go = delta / daily_average
-                        else:
-                            ghzdays_10 = int(float(get_500_level_stats(html_content=top_500_html, rank='10')) * 10000)
-                            if my_score < ghzdays_10:
-                                print('Your rank is between 25 and 10.')
-                                delta = (ghzdays_10 - my_score) / 10000
-                                days2go = delta / daily_average
-                            else:
-                                ghzdays_1 = int(float(get_500_level_stats(html_content=top_500_html, rank='1')) * 10000)
-                                if my_score < ghzdays_1:
-                                    print('Your rank is between 10 and 1.')
-                                    delta = (ghzdays_1 - my_score) / 10000
-                                    days2go = delta / daily_average
-                                else:
-                                    print("You must have done something REALLY naughty.\
-                                      You shouldn't see this output.")
-
-    print('At this rate you should surpass this rank in {} days.'.format(format(days2go, '.1f')))
-    print('NOTE:  Remember that they are probably increasing their "Total GHz Days" so the expected intersect day '
-          'will probably be further out in the furture than this predicted date.  If you are not increasing your '
-          'GHz Days as quickly as they are you will never reach them!')
+        next_rank = str(my_rank - 1)
+    next_rank_ghzdays = float(get_500_level_stats(html_content=top_500_html, rank=next_rank))
+    days2go = (next_rank_ghzdays - my_score) / daily_average
+    print('At this rate you should surpass the next Top 500 '
+          'rank, {}, in {} days.'.format(next_rank, format(days2go, '.1f')))
+    print("""###########################
+NOTE:  Remember that they are increasing their "Total GHz Days" too.
+The expected intersect date will probably be further out than this predicted date.
+###########################""")
 
 
 if __name__ == '__main__':
