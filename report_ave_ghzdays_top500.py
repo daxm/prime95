cb20e216cffd9a05b4f6d5a15509c4bbf0a102de
Dax Mickelson <3335783+daxm@users.noreply.github.com>
2017-12-19 14:33:56 -0700

    Made code more Pythonic.

--- report_ave_ghzdays_top500.py
+++ report_ave_ghzdays_top500.py
@@ -12,22 +12,20 @@ main_url = 'https://www.mersenne.org'
 # ### #
 
 
-def get_html_content(session, url, **kwargs):
-    if 'data' in kwargs:
-        data = kwargs['data']
-    else:
-        data = ''
-    if 'params' in kwargs:
-        params = kwargs['params']
-    else:
-        params = ''
+def get_html_content(session, url, data='', params=''):
+    """
+    :param session: required, the requests session.
+    :param url: required, the URL being queried.
+    :param data: optional, based on FORM style POST/PUT actions.
+    :param params: optional, based on variables passed as a part of the URL path (e.g. ?var1=1&var2=2)
+    :return: response.content
+    """
 
     html_response = session.get(url=url, params=params, data=data)
     if html_response.status_code == 200:
         return html_response.content
     else:
         print('Error with HTML GET of {}.  Status code {}.'.format(url, html_response.status_code))
-        return False
 
 
 def get_top500_data(html_content):
@@ -44,17 +42,15 @@ def get_top500_data(html_content):
             continue
         for gchild in child.children:  # This is the <tr> level.
             try:
-                counter = 0
-                for ggchild in gchild.children:  # This is the <td> level.
-                    if counter == 0:
+                for i, ggchild in enumerate(gchild.children):  # This is the <td> level.
+                    if i == 0:
                         rank = ggchild.text
-                    elif counter == 1:
+                    elif i == 1:
                         member_name = ggchild.text
-                    elif counter == 2:
+                    elif i == 2:
                         ghzdays = ggchild.text
                     else:
                         break
-                    counter += 1
                 line_entry = (str(gather_date), rank, member_name, ghzdays)
                 rank_data.append(line_entry)
             except AttributeError:
@@ -65,9 +61,10 @@ def get_top500_data(html_content):
 def main():
     with requests.Session() as session:
         # Grab the HTML for the pages we are going to parse.
-        top_500_html = get_html_content(session=session,
-                                        url='{}/report_top_500/'.format(main_url),
-                                        )
+        top_500_html = get_html_content(
+            session=session,
+            url='{}/report_top_500/'.format(main_url),
+        )
 
     # Collect the Top 500 table for comparison parsing vs. my stats.
     today_data = get_top500_data(html_content=top_500_html)
